458d7e76f34cfca39f6c3f2030929e7106b726e1
jaydwarka <2138668@talnet.nl>
2020-09-14 13:00:21 +0200

    change text from "Hallo klas 8A" to "Somebody once told me the world is gonna roll me                                      I ain't the sharpest tool in the shed                                      She was looking kind of dumb with her finger and her thumb                                      In the shape of an "L" on her forehead".

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -6,6 +6,9 @@ package h01;
 public class HalloRocDev {
 
     public static void main(String[] args) {
-        System.out.println("Hallo klas 8A!");
+        System.out.println("Somebody once told me the world is gonna roll me\n" +
+                "I ain't the sharpest tool in the shed\n" +
+                "She was looking kind of dumb with her finger and her thumb\n" +
+                "In the shape of an \"L\" on her forehead!");
     }
 }
