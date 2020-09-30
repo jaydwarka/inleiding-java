4f523469bbf55ec9a4b1caefe8023871c9dc443f
jaydwarka <2138668@talnet.nl>
2020-09-30 16:51:26 +0200

    changed some things in Figuren.java

--- src/h04/Figuren.java
+++ src/h04/Figuren.java
@@ -13,21 +13,46 @@ public class Figuren extends Applet {
         g.setColor(Color.black);
 
         //lijn
-        g.drawLine(40,30,200,30);
-        g.drawString("lijn",100,45);
+        g.drawLine(40, 30, 200, 30);
+        g.drawString("lijn", 100, 45);
 
         //rechthoek
-        g.drawRect(210,30,100,40);
-        g.drawString("rechthoek",230,85);
+        g.drawRect(210, 30, 100, 40);
+        g.drawString("rechthoek", 230, 85);
 
         //ovaal met kleur
         g.setColor(Color.magenta);
-        g.drawRect(60,60,100,40);
-        g.fillRect(60,60,100,40);
+        g.drawRect(60, 60, 100, 40);
+        g.fillRect(60, 60, 100, 40);
         g.setColor(Color.black);
-        g.drawOval(60,60,100,40);
-        g.drawString("ovaal met kleur",70,115);
+        g.drawOval(60, 60, 100, 40);
+        g.drawString("ovaal met kleur", 70, 115);
+
+        //afgeronde rechthoek
+        g.setColor(Color.black);
+        g.drawRoundRect(60,120,100,40,20,20);
+        g.drawString("afgeronde rechthoek",60,175);
+
+        //gevulde ovaal
+        g.setColor(Color.magenta);
+        g.drawOval(210,90,100,40);
+        g.fillOval(210,90,100,40);
+        g.setColor(Color.black);
+        g.drawString("gevulde ovaal",220,145);
+
+        //cirkel
+        g.setColor(Color.black);
+        g.drawOval(60,185,60,60);
+        g.drawString("cirkel",75,260);
+
+        //taartpunt met ovaal eromheen
+        g.drawArc(210,160,100,50,90,45);
+        g.setColor(Color.magenta);
+        g.fillArc(210,160,100,50,90,45);
+        g.setColor(Color.black);
+        g.drawOval(210,160,100,50);
+        g.drawString("taartpunt met ovaal eromheen",210,225);
 
 
     }
-}
+ }
