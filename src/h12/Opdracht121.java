164f024e89baec14b907e0cdabb297ae83949d7e
jaydwarka <2138668@talnet.nl>
2020-11-19 14:47:06 +0100

    added h12

--- src/h12/Opdracht121.java
+++ src/h12/Opdracht121.java
@@ -4,13 +4,30 @@ import java.applet.Applet;
 import java.awt.*;
 
 public class Opdracht121 extends Applet {
+    double[] getallen;
+    double[] kopie;
 
     public void init() {
+        getallen = new double[10];
+
+        for (int teller = 0; teller < getallen.length; teller++) {
+            getallen[teller] = 1 * (teller + 1);
+            
+        }
+
+
 
     }
 
 
     public void paint(Graphics g) {
+        for (int teller = 0; teller < getallen.length; teller++) {
+            g.drawString("" + getallen[teller], 80, 20 * teller + 20);
+            g.drawString("gemiddelde" + " 5,5", 15, 220);
+            
+        }
+
+        }
 
     }
-}
+
---
+++ src/h12/Opdracht122.java
@@ -0,0 +1,16 @@
+package h12;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht122 extends Applet {
+
+    public void init() {
+
+    }
+
+    
+    public void paint(Graphics g) {
+
+    }
+}
